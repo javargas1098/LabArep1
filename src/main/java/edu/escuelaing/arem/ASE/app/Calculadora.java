b6bcf68660547f257518998dab0dcb4fabf423f6
unknown <ja.vargas1098@gmail.com>
2019-08-09 12:32:05 -0500

    first commit

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/Calculadora.java
@@ -0,0 +1,14 @@
+package edu.escuelaing.arem.ASE.app;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculadora 
+{
+    public static void main( String[] args ) throws IOException
+    {
+    	FileReader fr = new FileReader ("data.txt");
+    	BufferedReader br = new BufferedReader(fr);
+    }
+}
